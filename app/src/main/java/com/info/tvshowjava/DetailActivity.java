331fd0c661208de7da39b70f431c81853042bd94
isikkadir <78980888+isikkadir@users.noreply.github.com>
2021-04-15 21:14:23 +0300

    set Indicator

--- app/src/main/java/com/info/tvshowjava/DetailActivity.java
+++ app/src/main/java/com/info/tvshowjava/DetailActivity.java
@@ -1,14 +1,18 @@
 package com.info.tvshowjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.info.tvshowjava.adapters.ImageAdapter;
 import com.info.tvshowjava.databinding.ActivityDetailBinding;
@@ -19,11 +23,14 @@ import com.info.tvshowjava.viewmodels.TvShowsDetailsViewModel;
 
 import java.util.List;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class DetailActivity extends AppCompatActivity {
     private ActivityDetailBinding activityDetailBinding;
     private TvShowsDetailsViewModel tvShowsDetailsViewModel;
     private PopularTvShows comingPopularTvShows;
     private ImageAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,16 +47,56 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void getTvShowsDetails(PopularTvShows comingPopularTvShows) {
-        tvShowsDetailsViewModel.getTvShowsDetails_viewModel(comingPopularTvShows.getId()).observe(this, TvShowDetails ->{
-            Log.e("Getirildi" ,"Getirilen : " + TvShowDetails.getTvShow().getUrl());
+        tvShowsDetailsViewModel.getTvShowsDetails_viewModel(comingPopularTvShows.getId()).observe(this, TvShowDetails -> {
+            Log.e("Getirildi", "Getirilen : " + TvShowDetails.getTvShow().getUrl());
             loadImages(TvShowDetails.getTvShow().getPictures());
 
         });
     }
-    private void loadImages(String[] images){
+
+    private void loadImages(String[] images) {
         activityDetailBinding.sliderViewPager.setOffscreenPageLimit(1);
         activityDetailBinding.sliderViewPager.setAdapter(new ImageAdapter(images));
         activityDetailBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityDetailBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicator(images.length);
+        activityDetailBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
+    }
+
+    private void setupSliderIndicator(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                    R.drawable.slider_images_deactive));
+            indicators[i].setLayoutParams(layoutParams);
+            activityDetailBinding.linearLayout.addView(indicators[i]);
+        }
+        activityDetailBinding.linearLayout.setVisibility(View.VISIBLE);
+        setCurrentSliderIndicator(0);
+    }
+
+    private void setCurrentSliderIndicator(int position) {
+        int childCount = activityDetailBinding.linearLayout.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) activityDetailBinding.linearLayout.getChildAt(i);
+            if (i == position) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(),
+                                R.drawable.slider_images_active)
+                );
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                        R.drawable.slider_images_deactive));
+            }
+        }
     }
 }
